e3150acd1e3311fc8902ed1ce9c489efaee68f42
PoteznyDominator <b.murawski00@gmail.com>
2021-09-12 13:31:38 +0200

    Add funtionality to buttons and search box, add hide API key and catch error from fetch

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-const date_text =                document.querySelector('.date');
-const degrees_text =             document.querySelector('.degrees');
-const location_text =            document.querySelector('.location');
-const desc_text =                document.querySelector('.desc');
-const type_degree =              document.querySelector('.degree-type');
+// to hide API key
+import WEATHER_API_KEY from "./config.js";
 
-generateWeather('warszawa').then(response => {
-  setInterval(function () {showTime(response, date_text)}, 1000);
+const search_btn    =             document.querySelector('.search');
+const search_input  =             document.querySelector('.city-input');
 
-  type_degree.addEventListener('click', () => {
-    if(type_degree.checked) {
-      degrees_text.innerHTML = converTemp(response.main.temp, "Fahrenheit");
-    } else {
-      degrees_text.innerHTML = converTemp(response.main.temp, "Celcius");
-    }
-  });
-  degrees_text.checked = true;
-  degrees_text.innerHTML = converTemp(response.main.temp, "Celcius");
-  desc_text.innerHTML = response.weather[0].description;
-  // to recognize country name
-  let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
-  location_text.innerHTML = `${response.name}, ${regionNames.of(response.sys.country)}`;
+const date_text     =             document.querySelector('.date');
+const degrees_text  =             document.querySelector('.degrees');
+const location_text =             document.querySelector('.location');
+const desc_text     =             document.querySelector('.desc');
+const type_degree   =             document.querySelector('.degree-type');
+let   degree_value  =             "";       
+
+// created to delete unnecessary intervals
+let interval_id = "";
+window.onload = requestCall('London');
+
+search_btn.addEventListener('click', async () => {
+  if(search_input.value === ""){
+    return
+  }
+
+  await requestCall(search_input.value);
+});
+
+search_input.addEventListener('keypress', async (e) => {
+  if(search_input.value === ""){
+    return
+  }
+
+  if(e.key === 'Enter') {
+    await requestCall(search_input.value);
+  }
+});
+
+type_degree.addEventListener('click', () => {
+  if(type_degree.checked) {
+    degrees_text.innerHTML = converTemp(degree_value, "Fahrenheit");
+  } else {
+    degrees_text.innerHTML = converTemp(degree_value, "Celcius");
+  }
 });
 
 function showTime(response, date_text) {
@@ -60,10 +79,52 @@ function converTemp(value, type) {
   }
 }
 
-async function generateWeather(city) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=182090554f2a3753ab4e05f6b5343b36`, {mode: 'cors'})
-  const weather = await response.json();
-  
-  return weather
+async function errorHandle() {
+
 }
 
+async function generateWeather(city) {
+  try {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}`, {mode: 'cors'});
+    let weather = await response.json().catch();
+    
+    return weather
+  } catch (error) {
+    return error
+  }
+}
+
+// to set delay
+const sleep = m => new Promise(r => setTimeout(r, m))
+
+async function requestCall (cityName) {
+  let response = await generateWeather(cityName);
+  
+  if (response.cod == 404) {
+    console.error(response.message);
+    search_input.value = "";
+    const search = document.querySelector('.search-container');
+    let default_shadow = search.style.boxShadow;
+    search.style.boxShadow = "0px 0px 10px 3px rgba(255,0,0,0.5)";
+    await sleep(2000);
+    search.style.boxShadow = default_shadow;
+    return
+  }
+
+  if(interval_id != ""){
+    window.clearInterval(interval_id);
+  }
+
+  interval_id = window.setInterval(function () {showTime(response, date_text)}, 1000);  
+  
+  degrees_text.checked = true;
+  degree_value = response.main.temp;
+  degrees_text.innerHTML = converTemp(response.main.temp, "Celcius");
+  
+  desc_text.innerHTML = response.weather[0].description;
+  // to recognize country name
+  let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+  location_text.innerHTML = `${response.name}, ${regionNames.of(response.sys.country)}`;
+  
+  search_input.value = "";
+}
